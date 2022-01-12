a466fc44a0c5d0e4b1552bbaf6357bcaa36c3b18
Duc Tran <ductran.tpc@gmail.com>
2022-01-12 22:19:20 +0700

    first

---
+++ src/main/java/Helloworld.java
@@ -0,0 +1,6 @@
+public class Helloworld {
+    public static void main(String[] args){
+        Person superman = new Person("superman", 32);
+        System.out.println("I-am a " + superman.getName());
+    }
+}
