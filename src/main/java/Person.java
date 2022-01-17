2aa66920615feb865dd2d1bccdf03fdaa2292036
Duc Tran <ductran.tpc@gmail.com>
2022-01-17 12:55:02 +0700

    test

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,24 +1,49 @@
 public class Person {
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 
     private String name;
     private int age;
+
+    /**
+     * This constructor is used for create new person with name and age
+     * @param name : a String present name of person
+     * @param age : int
+     */
     public Person(String name, int age){
         this.name = name;
         this.age = age;
     }
+
+    /**
+     * Get name of person
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * set name of person with given name
+     * @param name : String
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Get age of person
+     * @return age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * Set age of person with given age
+     * @param age : int
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
 }
